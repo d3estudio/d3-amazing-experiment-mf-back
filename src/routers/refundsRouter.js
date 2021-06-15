a28e810cb9a4f9bdda931191b9c7d06444c40250
KlaussVP <klauss_mbc@hotmail.com>
2021-06-15 14:51:03 -0300

    feat: refunds list route

--- src/routers/refundsRouter.js
+++ src/routers/refundsRouter.js
@@ -19,7 +19,20 @@ router.get('/date', verifyToken, (req, res) => {
 });
 
 router.get('/list', verifyToken, async (req, res) => {
-  
+  const token = req.token;
+  const year = req.query.year;
+  const month = req.query.month;
+
+  if (!year || !month) return res.status(400).json({ error: 'Período não especificado corretamente.'});
+
+  axios.get(`${process.env.BASE_URL}/v1/refunds/${year}/${month}/?token=${token}`)
+  .then(response => {
+    return res.send(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+    return res.sendStatus(500);
+  });
 });
 
 router.get('/expenses', verifyToken, async (req, res) => {
